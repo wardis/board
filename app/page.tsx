d56c8e0cd7c68aaa365d8f26cc77de497dee12a9
Wardi Soulaimana <wardi@wardi-s.com>
2024-09-14 06:37:04 +0100

    add authentication loading icon

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
-import { Button } from '@/components/ui/button'
+import { UserButton } from '@clerk/nextjs'
 
 export default function Home() {
   return (
-    <div>
-      <p>Hello</p>
-      <Button size="sm" variant="destructive">
-        Click Me
-      </Button>
+    <div className="flex flex-col gap-y-4">
+      <p>This screen is for authenticated users</p>
+      <UserButton />
     </div>
   )
 }
