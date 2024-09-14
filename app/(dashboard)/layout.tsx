3c5ffefbac5a3ff0386df60f8a117c9dd6c5ffb1
Wardi Soulaimana <wardi@wardi-s.com>
2024-09-14 07:03:37 +0100

    create base layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,4 +1,6 @@
+import { OrgSidebar } from './_components/org-sidebar'
 import { Sidebar } from './_components/sidebar'
+import { Navbar } from './_components/sidebar/navbar'
 
 export default function DashboardLayout({
   children,
@@ -8,7 +10,15 @@ export default function DashboardLayout({
   return (
     <main className="h-full">
       <Sidebar />
-      <div className="pl-[60px] h-full">{children}</div>
+      <div className="pl-[60px] h-full">
+        <div className="flex gap-x-3 h-full">
+          <OrgSidebar />
+          <div className="h-full flex-1">
+            <Navbar />
+            {children}
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
